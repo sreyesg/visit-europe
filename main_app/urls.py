133d756fd933d6b24274a97a0f4e027c93280745
sreyeg <santiagoreyes.g@gmail.com>
2025-02-01 21:35:45 -0800

    Add read feature and landing.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from main_app.views import ArticleCreate
+from main_app.views import ArticleCreate, ArticleList
 
 urlpatterns = [
-    path('', views.landing, name='landing'),
+    path('', views.ArticleList.as_view(), name='landing'),
     path('articles/create', views.ArticleCreate.as_view(), name='article_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 from main_app.models import Article
 
-def landing(request):
-    return render(request,'base.html')
-
+class ArticleList(ListView):
+    model = Article
+    template_name = 'lading.html'
+    context_object_name = 'articles'
+    queryset = Article.objects.all()
+    
 
 
 class ArticleCreate(CreateView):
