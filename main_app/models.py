05ddba6958a84da4cf2cb6b4eebf7271fb0afc45
sreyeg <santiagoreyes.g@gmail.com>
2025-02-01 17:27:27 -0800

    Define User and Article models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Article(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField(max_length=250)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
